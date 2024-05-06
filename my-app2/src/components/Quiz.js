c80db687e8f01c864ffd182f70a83a85aad25c75
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-06 19:24:27 +0500

    changes

--- my-app2/src/components/Quiz.js
+++ my-app2/src/components/Quiz.js
@@ -1,27 +1,31 @@
-import React,{useState} from 'react'
-import QUESTIONS from "./Questions.js"
+import React, { useState } from 'react';
+import QUESTIONS from './Questions.js';
 
 export default function Quiz() {
-const {UserAnswers,setUserAnswers} = useState([]) 
+  const [UserAnswers, setUserAnswers] = useState([]);
 
+  function HandleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAns) => {
+      return [...prevUserAns, selectedAnswer];
+    });
+  }
 
-function HandleSelectAnswer(selectedAnswer){
- setUserAnswers((prevUserAns=>{
-    return [...prevUserAns,selectedAnswer]
- }))
-}
+  const ActiveQuestionIndex = UserAnswers.length;
 
-const ActiveQuestionIndex = UserAnswers.length;
-  return (
-    <div id="question">
-    <h1>{QUESTIONS[ActiveQuestionIndex].text}</h1>
-    <ul id="answers">
-        {QUESTIONS[ActiveQuestionIndex].answers.map(answer=>{
-        <li key={answer} className='answer'>
-            <button onClick={HandleSelectAnswer}>{answer}</button>
-        </li>
-        })}
-    </ul>
-    </div>
-  )
+  // Check if QUESTIONS[ActiveQuestionIndex] exists before rendering
+  if (QUESTIONS[ActiveQuestionIndex]) {
+    return (
+      <div id="question">
+        <h1>{QUESTIONS[ActiveQuestionIndex].text}</h1>
+        <ul id="answers">
+          {QUESTIONS[ActiveQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              
+              <button onClick={() => HandleSelectAnswer(answer)}>{answer}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  } 
 }
