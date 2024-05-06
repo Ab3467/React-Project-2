9cecc08c08dd8c9c2293b34d739182251eef8df2
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-06 21:05:39 +0500

    changes

--- my-app2/src/components/QuestionTimer.js
+++ my-app2/src/components/QuestionTimer.js
@@ -1,6 +1,9 @@
 import React,{useState} from 'react'
 
 export default function QuestionTimer({timeout,onTimeOut}) {
+
+    const [remainingTime,setremainingTime] = useState();
+
     setTimeout(onTimeOut,timeout);
   return (
     <progress id="question-time"/>
