d58e478d0be7a653337015b6e2341986df83cbd9
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-06 21:15:37 +0500

    changes

--- my-app2/src/components/QuestionTimer.js
+++ my-app2/src/components/QuestionTimer.js
@@ -1,10 +1,19 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 
 export default function QuestionTimer({timeout,onTimeOut}) {
 
     const [remainingTime,setremainingTime] = useState(timeout);
 
-    setTimeout(onTimeOut,timeout);
+    useEffect(() => {
+        setTimeout(onTimeOut,timeout);
+    }, [])
+    
+    useEffect(()=>{
+        setInterval(() => {
+            setremainingTime(prevRemainingTime=> prevRemainingTime-100);
+        }, 100);
+    },[])
+    
   return (
     <progress id="question-time"/>
   )
