d287f6c818f6f32a1434da60c66ae978af33f547
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-06 18:49:55 +0500

    changes

--- my-app2/src/components/Quiz.js
+++ my-app2/src/components/Quiz.js
@@ -2,6 +2,8 @@ import React,{useState} from 'react'
 
 export default function Quiz() {
 const {Answers,setAnswers} = useState(0) 
+
+const ActiveQuestionIndex = Answers.length;
   return (
     <div id="question">
     <p>Currently Active Questions</p>
