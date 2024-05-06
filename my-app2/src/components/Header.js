a92db73e234ff879495d2c8bf30c22349c3e503e
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-06 18:35:47 +0500

    changes

--- my-app2/src/components/Header.js
+++ my-app2/src/components/Header.js
@@ -5,7 +5,7 @@ import Image from "../assets/Quiz.jpg"
 export default function Header() {
   return (
     <header>
-        <img src={Image}/>
+        <img src={Image} alt='quiz-logo'/>
         <h1>React Quiz</h1>
     </header>
   )
