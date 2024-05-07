6fc6648a1a706712352cf77c6837dcbd4bdd2881
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-07 18:28:02 +0500

    Changes

--- my-app2/src/components/QuestionTimer.js
+++ my-app2/src/components/QuestionTimer.js
@@ -1,25 +1,23 @@
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
 
-export default function QuestionTimer({timeout,onTimeOut}) {
+export default function QuestionTimer({ timeout, onTimeOut }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
 
-    const [remainingTime,setremainingTime] = useState(timeout);
+  useEffect(() => {
+    const timer = setTimeout(onTimeOut, timeout);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeout, onTimeOut]);
 
-    useEffect(() => {
-        const timer = setTimeout(onTimeOut,timeout);
-        return ()=>{
-            clearTimeout(timer)
-        }
-    }, [timeout,onTimeOut])
-    
-    useEffect(()=>{
-        setInterval(() => {
-            const interval = setremainingTime(prevRemainingTime=> prevRemainingTime-100);
-        }, 100);
-        return ()=>{
-            clearInterval(interval);
-        };
-    },[])
-    
-  return 
-    <progress id="question-time" max={timeout} value={remainingTime}/>
-  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+    }, 100);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <progress id="question-time" max={timeout} value={remainingTime} />;
+}
