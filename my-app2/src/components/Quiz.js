874b451e1bcd91bc9c8585420d5a8d49158b09e3
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-07 18:50:47 +0500

    Changes

--- my-app2/src/components/Quiz.js
+++ my-app2/src/components/Quiz.js
@@ -35,14 +35,16 @@ shuffledAnswers.sort(()=> Math.random()- 0.5)
 return (
     <div id="quiz">
       <div id="question">
-      <QuestionTimer timeout={10000} onTimeOut={HandleSkipAnswer} />
+      <QuestionTimer
+      key={activeQuestionIndex}
+      timeout={10000} onTimeOut={HandleSkipAnswer} />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-            {shuffledAnswers.map((answer=>(
+            {shuffledAnswers.map((answer)=>(
          <li key={answer} className='answer'>
-        <button onClick={HandleSelectAnswer}>{answer}</button>
+        <button onClick={()=>HandleSelectAnswer(answer)}>{answer}</button>
         </li>
-            )))}
+            ))}
          
         </ul>
       </div>
