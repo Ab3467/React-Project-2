f85363daa0a5e6cb563839f322b811adf3b12a0b
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-09 18:32:08 +0500

    changes

--- my-app2/src/components/Question.js
+++ my-app2/src/components/Question.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
 import QuestionTimer from './QuestionTimer'
 import Answers from './Answers'
 
 export default function Question({questionText,answers,onSelectAnswer,selectedAnswer,AnswerState,onSkipAnswer}) {
+    const [answer,setAnswer] = useState({
+        selectedAnswer:'',
+        isCorrect: null,
+    })
   return (
     <div id="question">
     <QuestionTimer timeout={10000} onTimeOut={onSelectAnswer} />
