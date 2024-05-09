395963565ff769722bb824cf824c28c43f95a93a
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-09 19:09:57 +0500

    changes

--- my-app2/src/components/Quiz.js
+++ my-app2/src/components/Quiz.js
@@ -17,8 +17,8 @@ export default function Quiz() {
      setUserAnswers((prevAnswers) => {
      return [...prevAnswers, selectAnswer];
      });
-  }, [activeQuestionIndex]
-);
+  }, 
+  []);
   
   
   
